d8e52e3558f2439d83e7496ddafc3ba6ac277b60
Alec J <alecaej2002@gmail.com>
2017-10-24 21:38:31 -0700

    added homepage

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -1,4 +1,8 @@
 class MessengerController < ApplicationController
 	def receive_message
+
+	end
+
+	def homepage
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'messenger#homepage'
   post 'messenger/webhook' => 'messenger#receive_message'
+
 end
