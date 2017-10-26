fbe8ea0adfd8bd4b89a7ffabd561d632f5b251a4
Alec J <alecaej2002@gmail.com>
2017-10-25 21:08:23 -0700

    trying to send message

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -1,8 +1,26 @@
 class MessengerController < ApplicationController
 	protect_from_forgery with: :null_session
+	require 'httparty'
+	require 'json'
 	def receive_message
 		$webhook = request.raw_post
 		puts $webhook.inspect
+		if !$webhook.nil?
+
+
+
+			# send message
+			body = {
+	   		text: "hi there, you said trip"
+			}.to_json
+			response = HTTParty.post(
+	 			"https://hooks.slack.com/services/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",
+	 			body: body,
+	 			headers: { 'Content-Type' => 'application/json' }
+			)
+
+
+		end
 	end
 
 	def homepage
