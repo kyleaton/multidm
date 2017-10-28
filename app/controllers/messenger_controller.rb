2494369094264cb3acc911ec5e253ea95c8dc2f0
Alec J <alecaej2002@gmail.com>
2017-10-27 20:08:59 -0700

    added parameters

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -2,12 +2,18 @@ class MessengerController < ApplicationController
 	protect_from_forgery with: :null_session
 	require 'httparty'
 	require 'json'
-	
+
 	def receive_message
 		$webhook = request.raw_post
 		puts $webhook.inspect
 		if $webhook["token"] = "caPwPxIyw5qrXnunVr2cN10R"
-			Messagehuman.sendMessage("hey there, I'm Alec")
+			if $webhook["text"] == "insult"
+				Messagehuman.sendMessage("you ugly as poo")
+			elsif $webhook["text"] == "compliment"
+				Messagehuman.sendMessage("you're absolutely gorgeous")
+			else
+				Messagehuman.sendMessage("sorry, i don't understand")
+			end
 		end
 	end
 
