b1cafbb4d6c6fca06aaf866228470b8534b1c1ed
Alec J <alecaej2002@gmail.com>
2017-11-01 19:31:32 -0700

    trying to fix parser error

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -2,9 +2,10 @@ class MessengerController < ApplicationController
 	protect_from_forgery with: :null_session
 	require 'httparty'
 	require 'json'
+	require 'cgi'
 
 	def receive_message
-		@webhook = JSON.parse(request.raw_post)
+		@webhook = CGI::parse(request.raw_post)
 		$theText = @webhook
 		puts @webhook.inspect
 		if @webhook["token"] == "igdU33zedZ6zU7gevHrZDNWT"
