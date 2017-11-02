073d78ecfa59d8799dc20906428b3b15fc7e4579
Alec J <alecaej2002@gmail.com>
2017-11-01 19:22:13 -0700

    created app /ttyl

--- app/models/messagehuman.rb
+++ app/models/messagehuman.rb
@@ -7,7 +7,7 @@ class Messagehuman
 	   		text: message
 			}.to_json
 			response = HTTParty.post(
-	 			"https://hooks.slack.com/services/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",
+	 			"https://hooks.slack.com/services/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",
 	 			body: body,
 	 			headers: { 'Content-Type' => 'application/json' }
 			)
