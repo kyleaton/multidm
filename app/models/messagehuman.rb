ccc4ee9c06df6dd01453f71f3fad6ecdbc7f7547
Alec J <alecaej2002@gmail.com>
2017-11-02 21:41:05 -0700

    stuff

--- app/models/messagehuman.rb
+++ app/models/messagehuman.rb
@@ -3,14 +3,14 @@ class Messagehuman
 
 	def self.sendMessage(message)
 		# send message
-			body = {
-	   		text: message
-			}.to_json
-			response = HTTParty.post(
-	 			"https://hooks.slack.com/services/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",
-	 			body: body,
-	 			headers: { 'Content-Type' => 'application/json' }
-			)
-		end
+		body = {
+   		text: message
+		}.to_json
+		response = HTTParty.post(
+ 			"https://hooks.slack.com/services/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",
+ 			body: body,
+ 			headers: { 'Content-Type' => 'application/json' }
+		)
+	end
 
 end
