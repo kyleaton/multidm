29b1a53eb03213406124d90520102844ac24fb1a
Alec J <alecaej2002@gmail.com>
2017-11-11 09:23:14 -0800

    trying to access userlist hash thingy

--- app/models/messagehuman.rb
+++ app/models/messagehuman.rb
@@ -13,11 +13,4 @@ class Messagehuman
 		)
 	end
 
-	def self.userlist()
-		# send message
-		response = HTTParty.get(
- 			"https://slack.com/api/users.list?token=xoxp-219592720864-220173653139-265033877552-f3a1fd016fbe6ed8c63c85c0ec52ead4"
-		)
-	end
-
 end
