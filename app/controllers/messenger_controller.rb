0c3c0cfb4a5883b2a3ac0a2af5b4cf988a997055
Alec J <alecaej2002@gmail.com>
2017-11-11 10:44:51 -0800

    outputting user di

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -9,7 +9,10 @@ class MessengerController < ApplicationController
 		puts @webhook.inspect
 		if @webhook["token"][0] == "igdU33zedZ6zU7gevHrZDNWT"
 			@userList = HTTParty.get("https://slack.com/api/users.list?token=xoxp-219592720864-220173653139-265033877552-f3a1fd016fbe6ed8c63c85c0ec52ead4")
- 			puts @userList.parsed_response["members"].inspect
+ 			@userList = @userList.parsed_response["members"][].inspect
+ 			@userList.each do |user|
+ 				puts user["id"]
+ 			end
  		end
 	end
 
