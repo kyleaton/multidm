38926c52feecc4595cbe3b9518acc8b049c66610
Alec J <alecaej2002@gmail.com>
2017-11-10 21:28:24 -0800

    successfully outputting users list

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -7,11 +7,11 @@ class MessengerController < ApplicationController
 	def receive_message
 		@webhook = CGI::parse(request.raw_post)
 		puts @webhook.inspect
-		#if @webhook["token"][0] == "igdU33zedZ6zU7gevHrZDNWT"
+		if @webhook["token"][0] == "igdU33zedZ6zU7gevHrZDNWT"
 			# does the token change each time? what token do i use? 
 			response = HTTParty.get("https://slack.com/api/users.list?token=xoxp-219592720864-220173653139-265033877552-f3a1fd016fbe6ed8c63c85c0ec52ead4")
  			puts response.inspect
- 		#end
+ 		end
 	end
 
 	def homepage
