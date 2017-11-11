4d6482fbfd0a4aa6cfa0cde6335b3247e4ace55e
Alec J <alecaej2002@gmail.com>
2017-11-11 11:27:27 -0800

    spelt message with 3 sss oops

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -8,14 +8,14 @@ class MessengerController < ApplicationController
 		@webhook = CGI::parse(request.raw_post)
 		puts @webhook.inspect
 		if @webhook["token"][0] == "igdU33zedZ6zU7gevHrZDNWT"
-			findTeam = Messsage.where(team_id: @webhook["team_id"])
+			findTeam = Message.where(team_id: @webhook["team_id"])
 			if findTeam.empty?
 				@userList = HTTParty.get("https://slack.com/api/users.list?token=xoxp-219592720864-220173653139-265033877552-f3a1fd016fbe6ed8c63c85c0ec52ead4")
 	 			@userList = @userList.parsed_response["members"]
 	 			puts @userList.inspect
 	 			@userList.each do |user|
 	 				puts user["id"] + " - " + user["team_id"] + " - " + user["name"]
-	 				newUser = Messsage.new()
+	 				newUser = Message.new()
 	 				newUser.save(user_id: user["id"], team_id: user["team_id"], user_name: user["name"])
 	 			end
  			end
