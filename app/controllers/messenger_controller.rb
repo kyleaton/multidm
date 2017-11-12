78123823dfbb53756e36c01c8444a819d9204475
Alec J <alecaej2002@gmail.com>
2017-11-11 19:10:06 -0800

    inspecting var

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -8,6 +8,7 @@ class MessengerController < ApplicationController
 		@webhook = CGI::parse(request.raw_post)
 		puts @webhook.inspect
 		if @webhook["token"][0] == "igdU33zedZ6zU7gevHrZDNWT"
+
 			findTeam = Message.where(team_id: @webhook["team_id"])
 			if findTeam.empty?
 				@userList = HTTParty.get("https://slack.com/api/users.list?token=xoxp-219592720864-220173653139-265033877552-f3a1fd016fbe6ed8c63c85c0ec52ead4")
@@ -20,9 +21,21 @@ class MessengerController < ApplicationController
 	 			end
  			end
 
- 		@dmList = HTTParty.get("https://slack.com/api/im.list?token=xoxp-219592720864-220173653139-265033877552-f3a1fd016fbe6ed8c63c85c0ec52ead4")	
- 		@dmList = @dmList.parsed_response["ims"]
- 		puts @dmList.inspect
+	 		@dmList = HTTParty.get("https://slack.com/api/im.list?token=xoxp-219592720864-220173653139-265033877552-f3a1fd016fbe6ed8c63c85c0ec52ead4")	
+	 		@dmList = @dmList.parsed_response["ims"]
+	 		puts @dmList.inspect
+
+	 		@userText = @webhook["text"][0]
+	 		@splitText = @userText.split(" ") if !@userText.nil?
+	 		@userToText = Array.new
+	 		@splitText.each do |word|
+	 			if word[0] == "@"
+	 				word.slice!(0)
+	 				@userToText.push(word)
+	 			end
+	 		end
+
+	 		puts @userToText.inspect
  		end
 	end
 
