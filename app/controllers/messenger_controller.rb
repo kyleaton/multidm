455a331716fe31c95321174abb49c5ff990454bc
Alec J <alecaej2002@gmail.com>
2017-11-12 12:56:28 -0800

    putsing code

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -72,6 +72,7 @@ class MessengerController < ApplicationController
 	def done
 		puts "THE PARAMS"
 		code = params["code"]
+		puts code
 		#HTTParty.get("https://slack.com/api/oauth.access?client_id=219592720864.265033610496&client_secret=d5062fd434690b7d86bf58f68f7dc9ea&code=#{code}")
 	end
 end
