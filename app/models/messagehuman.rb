a1fa41b507521a66292f3b967383fd56f72f8845
Alec J <alecaej2002@gmail.com>
2017-11-12 10:52:45 -0800

    testing

--- app/models/messagehuman.rb
+++ app/models/messagehuman.rb
@@ -6,7 +6,8 @@ class Messagehuman
 		body = {
 			token: "xoxp-219592720864-220173653139-265033877552-f3a1fd016fbe6ed8c63c85c0ec52ead4",
 			channel: "D6H418JNS",
-   		text: "hey there"
+   		text: "hey there",
+   		as_user: true
 		}.to_json
 		response = HTTParty.post(
  			"https://slack.com/api/chat.postMessage",
