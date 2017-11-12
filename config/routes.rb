bad77c66ac5be44544ddf31c42f070ded980526d
Alec J <alecaej2002@gmail.com>
2017-11-12 13:08:32 -0800

    stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'messenger#homepage'
-  get 'done' => 'messenger#done' 
+  get 'done' => 'messenger#done'
   post 'messenger/webhook' => 'messenger#receive_message'
+  post 'done' => 'messenger#done'
 
 end
