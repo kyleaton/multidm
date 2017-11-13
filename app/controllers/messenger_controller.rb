e063150883786772ee413d7b5680c845db1b790a
Alec J <alecaej2002@gmail.com>
2017-11-12 21:45:18 -0800

    stuff

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -29,7 +29,7 @@ class MessengerController < ApplicationController
 	 		puts @dmList.inspect
 
 	 		@userText = @webhook["text"][0]
-			@userText.gsub!("h", "&apos;")
+			@userText.gsub!("'", "h")
 			puts "THE USER TEXT"
 			puts @userText
 	 		if !@userText.nil?
