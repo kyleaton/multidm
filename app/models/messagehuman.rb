8e1cadd8656ff98abdcdfbb40a5741871d4c8730
Alec J <alecaej2002@gmail.com>
2017-11-12 23:01:49 -0800

    stuff

--- app/models/messagehuman.rb
+++ app/models/messagehuman.rb
@@ -4,9 +4,7 @@ class Messagehuman
 	def self.sendDM(channel, text, token)
 		text.gsub!("\u2019", "\'")
 		text.gsub!("\u201D", "\"")
-		#text.gsub!("&", "&amp;")
-		#text.gsub!("<", "&lt;")
-		#text.gsub!(">", "&gt;")
+		text.gsub!("&", "&#38;")
 		HTTParty.get("https://slack.com/api/chat.postMessage?token=#{token}&channel=#{channel}&text=#{text}&as_user=true")
 	end
 
