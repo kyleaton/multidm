b64a3966da8b4d23c560f6e103c9ada9e387fd30
Alec J <alecaej2002@gmail.com>
2017-11-12 22:03:37 -0800

    does this work

--- app/models/messagehuman.rb
+++ app/models/messagehuman.rb
@@ -3,7 +3,7 @@ class Messagehuman
 
 	def self.sendDM(channel, text, token)
 		str = "https://slack.com/api/chat.postMessage?token=#{token}&channel=#{channel}&text=#{text}&as_user=true"
-		str.gsub!("\u2019", "&apos;")
+		str.gsub!("\u2019", "\'")
 		HTTParty.get(str)
 	end
 
