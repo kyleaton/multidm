d8961a241f631d95e175194158fa76504422f479
Alec J <alecaej2002@gmail.com>
2017-11-12 22:41:51 -0800

    fixed it

--- app/models/messagehuman.rb
+++ app/models/messagehuman.rb
@@ -2,6 +2,7 @@ class Messagehuman
 	require 'json'
 
 	def self.sendDM(channel, text, token)
+		text.gsub!("\u2019s", "\'")
 		HTTParty.get("https://slack.com/api/chat.postMessage?token=#{token}&channel=#{channel}&text=#{text}&as_user=true")
 	end
 
